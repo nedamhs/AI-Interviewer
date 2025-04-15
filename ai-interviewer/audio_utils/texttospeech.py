a579ba5a708c58bc1f2e3a7b2f538a3dff35a951
Kevin Parfien <kparfien@uci.edu>
2025-04-15 15:27:37 -0700

    added comments to the text to speech

--- ai-interviewer/audio_utils/texttospeech.py
+++ ai-interviewer/audio_utils/texttospeech.py
@@ -5,10 +5,21 @@ import os
 
 load_dotenv()
 openai = AsyncOpenAI(api_key=os.environ.get("OPENAI-API-KEY"))
+# Needs ffmpeg installed
+# 
 
 
-async def text_to_audio(text: str):
-  '''Creates a stream of  audio'''
+async def text_to_audio(text: str) -> None:
+  '''
+  Creates an api stream to read a string
+
+      Inputs:
+        text: str 
+            text to serve as the input to the bot.
+      Returns: 
+        None
+  '''
+
   async with openai.audio.speech.with_streaming_response.create(
     model="tts-1",
     voice="alloy",
@@ -18,7 +29,17 @@ async def text_to_audio(text: str):
     await set_virtual_mic(response)
 
 
-async def set_virtual_mic(audio_bytes: bytes):
+async def set_virtual_mic(audio_bytes: bytes) -> None:
+    '''
+    Links the stream of bytes with the virtual mic and plays through it if it is set as the default device. 
+
+    Inputs:
+      audio_bytes: bytes 
+          pcm audio bytes that will be streamed to the bot. 
+    Returns: 
+      None
+    '''
+
     process = await asyncio.create_subprocess_exec(
         "ffplay",
         "-f", "s16le",       
