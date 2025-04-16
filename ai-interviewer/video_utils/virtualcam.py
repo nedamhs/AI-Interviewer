1387dba2108f56c4f4bc7b2c7e75d674a4b79a6d
ray16g <raymondh2003@yahoo.com>
2025-04-15 18:07:23 -0700

    Cleaned up virtual camera class

--- ai-interviewer/video_utils/virtualcam.py
+++ ai-interviewer/video_utils/virtualcam.py
@@ -4,15 +4,49 @@
 import pyvirtualcam
 from PIL import Image
 import numpy as np
+import threading
 
-# Start virtual camera
-def start_virtual_cam():
-    with pyvirtualcam.Camera(width=1280, height=720, fps=20) as cam:
-        print(f'Virtual Camera on {cam.device}')
-        image = np.array(Image.open("ai-interviewer/video_utils/test_image.png").convert('RGB'))
-        while True:
-            cam.send(image)
-            cam.sleep_until_next_frame()
+DEFAULT_WIDTH=1280
+DEFAULT_HEIGHT=720
+DEFAULT_FPS=20
+
+class VirtualCamera:
+    def __init__(self, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT, fps=DEFAULT_FPS):
+        self.video_width = width
+        self.video_height = height
+        self.video_fps = fps
+        self.current_frame = np.zeros((width, height, 3))
+        self.running = False
+        self.thread = None
+
+    def start_virtual_cam(self):
+        """" Start output stream for video into virtual camera """
+        with pyvirtualcam.Camera(width=self.video_width, height=self.video_height, fps=self.video_fps) as cam:
+            print(f'Virtual Camera on {cam.device}')
+            while self.running:
+                cam.send(self.current_frame)
+                cam.sleep_until_next_frame()
     
+    def set_frame(self, frame):
+        """ Set frame for virtual camera """
+        self.current_frame = frame
+
+    def video_start(self) -> None:
+        """ Starts transriber, starting the thread and video stream. """
+        self.running = True
+        self.thread = threading.Thread(target=self.start_virtual_cam())
+        self.thread.start()
+        print("Streaming video... Ctrl+C to stop.")
+        self.thread.join()
+
+    def video_stop(self) -> None:
+        """ Stops video stream. """
+        self.running = False
+
 if __name__ == "__main__":
-    start_virtual_cam()
+    # Test script
+    image = np.array(Image.open("ai-interviewer/video_utils/test_image.png").convert('RGB'))
+
+    virtual_cam = VirtualCamera(width=1280, height=720, fps=20)
+    virtual_cam.set_frame(image)
+    virtual_cam.video_start()
